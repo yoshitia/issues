5ec94041147c2fb9785f3bcaec7669902d91c8ca
yoshitia <naha.it01@gmail.com>
2019-06-21 09:43:36 +0900

    add httpoison

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -5,7 +5,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -27,6 +29,17 @@ defmodule Issues.CLI do
       _ -> :help
 
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch{user, project}
   end
 end
 
--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Issues.MixProject do
       app: :issues,
       version: "0.1.0",
       elixir: "~> 1.8",
+      build_embedded:  Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,6 +24,7 @@ defmodule Issues.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      { :httpoison, "~> 0.8" }
     ]
   end
 end
